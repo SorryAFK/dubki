ee6b8ecba11daa355d60370c0b67aa23701f83e7
pavelmacbook <pa2003sha@gmail.com>
2023-06-06 09:59:52 +0300

    Initial commit

---
+++ src/test/java/com/pzobenko/petproject/demo/SocialmediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pzobenko.petproject.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
